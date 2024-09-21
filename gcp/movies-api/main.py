16b57bf77857f326c542f3fda8e9f72413a5d72e
Diogo Guedes <diogo.guedes111@gmail.com>
2024-09-21 18:16:13 +0100

    set calls to the collection in default db

--- gcp/movies-api/main.py
+++ gcp/movies-api/main.py
@@ -1,10 +1,24 @@
 import firebase_admin
+from firebase_admin import credentials
 from firebase_admin import firestore
+import json
+# Use the application default credentials.
+cred = credentials.ApplicationDefault()
 
-# Application Default credentials are automatically created.
-app = firebase_admin.initialize_app()
+firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-movies_db = db.collection("movies")
 
-print(movies_db)
+
+
+with open('./data.json','r') as f:
+  # conver to list
+  data = json.load(f)
+  for row in data:
+    db.collection('movies').document(row['id']).set(
+      row
+  )
+  
+  
+
+
