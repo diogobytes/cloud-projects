17e7db33a4041bdd9f9957ce6fec28daacc238b5
Diogo Guedes <diogo.guedes111@gmail.com>
2024-09-27 21:37:07 +0100

    Initializing the destroy script file

---
+++ gcp/serverless-movies-api/destroy.sh
@@ -0,0 +1,9 @@
+# This script will be a one-shot script designed to fully destroy the entire environment.
+
+# Prerequisites
+# Authentication with: `gcloud auth application-default login`
+
+
+# Ensure script exits on any command failure
+set -e 
+
