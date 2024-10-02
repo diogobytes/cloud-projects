9354e6e5dc45fb45c3ecad82d87c37b6cad5c92f
Diogo Guedes <diogo.guedes111@gmail.com>
2024-10-02 19:27:22 +0200

    new cloud function enabling CORS

--- gcp/serverless-movies-api/cloud_functions_get_movies/main.py
+++ gcp/serverless-movies-api/cloud_functions_get_movies/main.py
@@ -4,21 +4,31 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 import json
 
+# Initialize Firebase Admin SDK with default application credentials
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred)
+
+# Define CORS headers
+CORS_HEADERS = {
+    "Access-Control-Allow-Origin": "*",  # Allow requests from any origin
+    "Access-Control-Allow-Methods": "GET",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Max-Age": "3600",
+    "Content-Type": "application/json",
+}
+
 
 @functions_framework.http
 def get_movies(request):
     """
     HTTP-triggered function to retrieve a list of movies from a Firestore database.
-
-    This function initializes the Firebase Admin SDK, connects to the Firestore
-    database, and retrieves all documents from the 'movies' collection, converting
-    them into a list of dictionaries. The list is then returned as a JSON response.
     """
-    try:
-        # Initialize Firebase Admin SDK with default application credentials
-        cred = credentials.ApplicationDefault()
-        firebase_admin.initialize_app(cred)
 
+    # Handle CORS preflight request
+    if request.method == "OPTIONS":
+        return ("", 204, CORS_HEADERS)
+
+    try:
         # Create a Firestore client
         db = firestore.client()
 
@@ -29,7 +39,7 @@ def get_movies(request):
         list_movies = [movie.to_dict() for movie in movies]
 
         # Return the list of movies as a JSON response
-        return json.dumps(list_movies), 200, {"Content-Type": "application/json"}
+        return json.dumps(list_movies), 200, CORS_HEADERS
 
     except Exception as e:
         # Log the error and return an error message as a JSON response
@@ -37,5 +47,5 @@ def get_movies(request):
         return (
             json.dumps({"error": "An error occurred while fetching movies"}),
             500,
-            {"Content-Type": "application/json"},
+            CORS_HEADERS,
         )
