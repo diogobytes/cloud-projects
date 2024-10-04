00a49417c1c6e35dd8b273120b3db9076c4c2aa4
Diogo Guedes <diogo.guedes111@gmail.com>
2024-10-04 08:51:09 +0100

    Filter movies based on release year

--- gcp/serverless-movies-api/frontend-serverless-movies/src/App.js
+++ gcp/serverless-movies-api/frontend-serverless-movies/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   const [data,setData]=useState([]);
-  
+  const [searchMovie, setSearchMovie] = useState('')
   const getData=()=>{
     fetch('https://us-central1-resolute-ascent-429512-h0.cloudfunctions.net/get_movies'
     ,{
@@ -24,24 +24,36 @@ function App() {
   }
   useEffect(()=>{
     getData()
-  },[])
+  },[]);
+
+  const filterMovies = data.filter((movie) =>  
+    movie.release_year.toLowerCase().includes(searchMovie.toLowerCase()))
+
   return (
     <div className="App">
       <h1>Favorite movies</h1>
+       {/* Search box */}
+       <input
+        type="text"
+        placeholder="Search for a movie by its release year..."
+        value={searchMovie}
+        onChange={(e) => setSearchMovie(e.target.value)} // Update search term as user types
+        className="search-box"
+      />
      {
-       data && data.length > 0 && data.map((item, index) => (
-        <div key={index}>
-          <p>Movie name: {item.title}</p>
-          <p>Director: {item.director}</p>
-          <p>Rating: {item.rating}</p>
-          <img src={item.image_url} width={200} height={300}/>
-          
-        
+     
+        filterMovies.length > 0 && filterMovies.map((item, index) => (
+        <div key={index} className='movie-card'>
+          <p className='movie-title'>Movie name: {item.title}</p>
+          <p className='movie-details'>Genre: {item.genre}</p>
+          <p className='movie-details'>Rating: {item.rating}</p>
+          <p className='movie-details'>Release year: {item.release_year}</p>
+          <p className='movie-details'>Director: {item.director}</p>
+          <img src={item.image_url}/>
         </div>
-      
       ))
      }
-    </div>
+     </div>
   );
 }
 
